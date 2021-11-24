5479d9ac4ef4c950f20df8f3ad015f84f5d2c16c
linbets <lindseybetsinger@gmail.com>
2021-11-24 10:26:46 -0600

    new files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <AboutMe/>
+      <Projects/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis aspernatur deserunt ducimus blanditiis tempore iure sunt cumque quam, impedit voluptates explicabo animi hic maxime facere nihil tempora officia corrupti non?
+        </div>
+    )
+}
