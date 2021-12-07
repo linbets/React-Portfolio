5c5aec079b0f494eb2863c61ad5d2afafdc7a54d
linbets <lindseybetsinger@gmail.com>
2021-12-07 11:10:54 -0600

    initial card setup

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,18 @@
 export default function About() {
     return(
-        <div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis aspernatur deserunt ducimus blanditiis tempore iure sunt cumque quam, impedit voluptates explicabo animi hic maxime facere nihil tempora officia corrupti non?
+        <div class="container" id="aboutme">
+        <div class="row">
+          <div class="col-3">
+            <h2>About Me</h2>
+          </div>
+
+          <div class="col-9" id="abouttext">
+            I am a certified full stack web developer with vast array of knowledge of both front end and back end languages. My objective is to further my knowledge and experience to become a skilled and expert developer.
+            <br></br>
+            My technical skills include JavaScript, HTML, CSS, Bootstrap, React, JQuery, and Node.
+            <br></br>
+          </div>
         </div>
+    </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,63 +1,21 @@
 export default function Projects() {
     return (
-        <>
+
+      
+  <div class="container" id="myprojects">
+    <div class="row">
+      <div class="col-3">
+        <h2>My Projects</h2>
+      </div>
         <div className="card border-primary mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
+  <div className="card-header">Nextflix</div>
   <div className="card-body">
     <h4 className="card-title">Primary card title</h4>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-<div className="card border-secondary mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Secondary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-success mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Success card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-danger mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Danger card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-warning mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Warning card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-info mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Info card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-light mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Light card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-dark mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Dark card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</>
-    )
 
-}
+</div>
+</div>
+    
+    ) }
