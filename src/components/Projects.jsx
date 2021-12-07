810a0a35fdf52e96ee542f07da8f42e961abe5a7
linbets <lindseybetsinger@gmail.com>
2021-12-07 11:49:11 -0600

    project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,13 +7,32 @@ export default function Projects() {
       <div class="col-3">
         <h2>My Projects</h2>
       </div>
-        <div className="card border-primary mb-3" style={{maxWidth: "20rem"}}>
-  <div className="card-header">Nextflix</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+    <div className="card border-primary mb-3" style={{maxWidth: "20rem"}}>
+      <div className="card-header">Nextflix</div>
+      <div className="card-body">
+      <h4 className="card-title">ladkjf</h4>
+      <p className="card-text"><a href="https://cleadi.github.io/nextflix/" target="_blank"> Deployed Site</a><br></br>
+      <a href="https://github.com/cleadi/nextflix/" target="_blank">Github Repository</a></p>
+      </div>
+    </div>
+
+    <div className="card border-primary mb-3" style={{maxWidth: "20rem"}}>
+      <div className="card-header">Bookstore</div>
+      <div className="card-body">
+      <h4 className="card-title">ladkjf</h4>
+      <p className="card-text"><a href="https://kesjoberg-bookstore.herokuapp.com/" target="_blank">Deployed Site</a><br></br>
+      <a href="https://github.com/Joe96/book-store" target="_blank">Github Repository</a></p>
+      </div>
+    </div>
+
+    <div className="card border-primary mb-3" style={{maxWidth: "20rem"}}>
+      <div className="card-header">True Crime Quiz</div>
+      <div className="card-body">
+      <h4 className="card-title">ladkjf</h4>
+      <p className="card-text"><a href="https://true-crime-quiz.herokuapp.com/" target="_blank">Deployed Site</a><br></br>
+      <a href="https://github.com/bretpeters3n/true-crime-quiz" target="_blank">Github Repository</a></p>
+      </div>
+    </div>
 
 </div>
 </div>
